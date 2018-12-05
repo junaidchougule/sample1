ef38212a4a57055f1c74bc32f6d78ceff25b7196
junaidchougule <555junaidchougule@gmail.com>
2018-12-05 14:47:45 +0530

    save3

--- src/main/java/com/scp/java/Maven/HibernateMapping_O2O/Address.java
+++ src/main/java/com/scp/java/Maven/HibernateMapping_O2O/Address.java
@@ -4,7 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 // first change
-// savevthbyh
+// savevthbyhkjs
 @Entity
 @Table(name ="add_info")
 public class Address {
